4b1ff500c9a00377f96286c45ceebfcaaacfbf9c
jegasape <jegasape@gmail.com>
2021-09-09 02:50:04 -0400

    1A17E3F62A86511534FFC045EFFA81C6

--- app.py
+++ app.py
@@ -1,19 +1,55 @@
+import asyncio
 import os
-import re
+import json
+import aiohttp
+import hashlib
 import requests
 from shutil import rmtree
+import time
 
-with open("urls.txt", "r") as f:
-    urls = f.readlines()
+start_time = time.time()
 
-for e in urls:
-    url = e.replace("\n", "")
-    directory = re.search(r'https://www.(.*?).com', e).group(1)
-    file_name = "{0}.txt".format(directory)
-    if(os.path.exists(directory)):
-        rmtree(directory)
-    else:
-        os.mkdir(directory)
-        rq = requests.get(url)
-        with open("{0}/{1}".format(directory, file_name), "wb") as f:
-            f.write(rq.content)
+
+async def main():
+    req = requests.get("https://api.cdnjs.com/libraries")
+    res = json.loads(req.content)["results"]
+    urls = [res[_]["latest"] for _ in range(len(res))]
+
+    async with aiohttp.ClientSession() as session:
+        for index, url in enumerate(urls):
+            try:
+                start_time_loop = time.time()
+                hash_file = hashlib.md5(url.encode())
+                file = hash_file.hexdigest()
+                link = url.replace("\n", "")
+                if(os.path.exists(file)):
+                    rmtree(file)
+                else:
+                    os.mkdir(file)
+                    async with session.get(link) as res:
+                        resp = await res.text()
+                        with open("{0}/{1}".format(file, f"{file}.txt"), "wb") as f:
+                            f.write(resp.encode())
+                print(
+                    f"{index} ---  {file[::-6]} --- {(time.time() - start_time_loop)} seconds ---")
+            except:
+                pass
+
+asyncio.run(main())
+print("--- %s seconds ---" % (time.time() - start_time))
+
+# Enter which files you want to download
+# with open("urls.txt", "r") as f:
+#     urls = f.readlines()
+
+# for e in urls:
+#     hash_file = hashlib.md5(e.encode())
+#     file = hash_file.hexdigest()
+#     url = e.replace("\n", "")
+#     if(os.path.exists(file)):
+#         rmtree(file)
+#     else:
+#         os.mkdir(file)
+#         rq = requests.get(url)
+#         with open("{0}/{1}".format(file, f"{file}.txt"), "wb") as f:
+#             f.write(rq.content)
